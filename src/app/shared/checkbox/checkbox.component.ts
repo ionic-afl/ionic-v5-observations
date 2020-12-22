a41fbb372f72bab53f1a97a84860b2457061f57b
Deepti Setti <M1053844@mindtree.com>
2020-12-22 17:46:02 +0530

    added ionic issues demo components

---
+++ src/app/shared/checkbox/checkbox.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkbox',
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.scss'],
+})
+export class CheckboxComponent implements OnInit {
+
+  public form = [
+    { val: 'Pepperoni', isChecked: false },
+    { val: 'Sausage', isChecked: false },
+    { val: 'Mushroom', isChecked: false }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { ToggleIssueComponent } from '../toggle-issue/toggle-issue.component';
+import { RadioButtonComponent } from '../radio-button/radio-button.component';
+import { DateComponent } from '../date/date.component';
+import { CheckboxComponent } from '../checkbox/checkbox.component';
+import { SelectComponent } from '../select/select.component';
 @NgModule({
-  declarations: [ToggleIssueComponent],
+  declarations: [ToggleIssueComponent, RadioButtonComponent, DateComponent, CheckboxComponent, SelectComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports:[ToggleIssueComponent]
+  exports:[ToggleIssueComponent, RadioButtonComponent, DateComponent, CheckboxComponent, SelectComponent]
 })
 export class SharedModule { }
