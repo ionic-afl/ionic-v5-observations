5645bd8c8ea19db73a190ce70d4e26ad5586859c
Deepti Setti <M1053844@mindtree.com>
2020-12-23 12:35:15 +0530

    added modal component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { ModalController, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { ModalComponent } from './shared/modal/modal.component';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private modalController: ModalController
   ) {
     this.initializeApp();
   }
@@ -23,5 +25,13 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  async openModal() {
+    const modal = await this.modalController.create({
+      component: ModalComponent,
+      cssClass: 'modal-class'
+    });
+    return await modal.present();
   }
 }
