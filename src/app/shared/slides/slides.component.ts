0aff2b03f9cb3f75b77334785417b1a54f629bdd
Deepti Setti <M1053844@mindtree.com>
2021-02-01 15:23:15 +0530

    added logger for ionic slide bug desription

--- src/app/shared/slides/slides.component.ts
+++ src/app/shared/slides/slides.component.ts
@@ -8,6 +8,8 @@ import { IonSlides } from '@ionic/angular';
 })
 export class SlidesComponent implements OnInit {
 
+  private currentIndex: number;
+
   constructor() { }
 
   ngOnInit() {}
@@ -20,6 +22,16 @@ export class SlidesComponent implements OnInit {
 
   prev() {
     this.slides.slidePrev();
+  }
+
+  /**
+   * @description handles active slide change
+   * This method will be executed whenver the slide changes
+   */
+  public async handleSlideChanges($event: Event) {
+    this.currentIndex = await ($event.target as any as IonSlides).getActiveIndex();
+    console.log(`current index of slides is [${this.currentIndex}]`);
+    console.log('while using swipe right in talkback,slide changes but this method will not be executed because ionSlideDidChange event is not emitted, which is a bug');
   }
 
 }
